60ebea0eb1692576d88954bf2d6d503ad8fd3b04
muhammmetikbalbayram <muhammetikbalbayram@gmail.com>
2024-04-30 15:29:26 +0200

    add new endpoints to server

--- blog-application-backend/src/server.js
+++ blog-application-backend/src/server.js
@@ -1,9 +1,47 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+        comments: []
+    },
+    {
+        name: 'learn-node',
+        upvotes: 0,
+        comments: []
+    },
+    {
+        name: 'mongodb',
+        upvotes: 0,
+        comments: []
+    },
+]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params
+    const article = articlesInfo.find(a => a.name === name)
+    if (article) {
+        article.upvotes++
+        res.send(`The "${name}" article has ${article.upvotes} upvotes!!`)
+    }else {
+        res.send("That article doesnt exist")
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { postedBy, text } = req.body
+    const { name } = req.params
+    const article = articlesInfo.find(a => a.name === name)
+    if (article) {
+        article.comments.push({postedBy,text})
+        res.send(article)
+    }else {
+        res.send("That article doesnt exist")
+    }
 });
 
 app.listen(8000, () => {
