b3f9f876bccada46d719456d235377965378e390
muhammmetikbalbayram <muhammetikbalbayram@gmail.com>
2024-05-03 15:52:26 +0200

    adding comments to articles

--- blog-application-frontend/src/pages/ArticlePage.js
+++ blog-application-frontend/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from "axios"
 import articles from "./article-content"
 import NotFoundPage from "./NotFoundPage"
 import CommentsList from "../components/CommentsList"
+import AddCommentForm from "../components/AddCommentForm"
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
@@ -37,6 +38,7 @@ const ArticlePage = () => {
                 {article.content.map((paragraph, i) => (
                     <p key={i}>{paragraph}</p>
                 ))}
+                <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
                 <CommentsList comments={articleInfo.comments} />
             </>
         )
