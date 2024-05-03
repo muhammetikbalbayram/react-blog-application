1aa5d913d81a05a5f7a04192984482bc25c95231
muhammmetikbalbayram <muhammetikbalbayram@gmail.com>
2024-05-03 16:30:56 +0200

    add firebase and simple login/create-account pages

--- blog-application-frontend/src/index.js
+++ blog-application-frontend/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9s-4kRH12bkL2-YQ3YFeubq3y7UGr_5A",
+  authDomain: "react-blog-application-2d94b.firebaseapp.com",
+  projectId: "react-blog-application-2d94b",
+  storageBucket: "react-blog-application-2d94b.appspot.com",
+  messagingSenderId: "39424009688",
+  appId: "1:39424009688:web:fbbd333e20ae0ae302bead"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
